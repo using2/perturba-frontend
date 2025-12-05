d93b8a109605089ee262efb5fb7f91474073a4bd
using6843@gmail.com <using6843@gmail.com>
2025-12-05 22:41:33 +0900

    feat: 게스트 로그인시, 다른 페이지 접근 방지

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useAuthStore } from "@/store/authStore";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
-    // const { isAuthenticated, loginType } = useAuthStore();
-    // const isGuest = loginType === "GUEST";
+    const pathname = usePathname();
+    const { isAuthenticated, loginType } = useAuthStore();
 
-    // useEffect(() => {
-    //     if (!isAuthenticated && !isGuest) {
-    //         router.replace("/");
-    //         return;
-    //     }
-    // }, [isAuthenticated, isGuest, router]);
+    const isGuest = loginType === "GUEST";
+    const guestAllowedPaths = ["/dashboard/image-upload"];
+    const isGuestAllowedPath = guestAllowedPaths.includes(pathname);
+
+    useEffect(() => {
+        if (!isAuthenticated && !isGuest) {
+            router.replace("/");
+            return;
+        }
+
+        if (isGuest && !isGuestAllowedPath) {
+            router.replace("/dashboard/image-upload");
+            return;
+        }
+    }, [isAuthenticated, isGuest, router]);
 
     return (
         <div className="flex-1 min-h-0 flex flex-col">
